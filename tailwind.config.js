f04e88a71486e895b85f3d96162ef7ad39420cb2
pranayakhadgi <pranaya.khadgi99@gmail.com>
2026-01-06 21:49:22 -0600

    added some tailwind config - spacing and divisions refactored

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pranayakhadgi.github.io/**/*.{html,js}",
+    "./pranayakhadgi.github.io/index.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
